9623f7bcc77c6e688de6ade071c52a19736ff666
George Holt <george_holt77@yahoo.co.uk>
2020-04-24 14:05:15 +0100

    improved logging and added features

---
+++ backup_config.py
@@ -0,0 +1,20 @@
+source_dirs = [
+    ('./test_data/source1', 'f1'),
+    ('./test_data/source2', 'f2')
+]
+
+backup_locs = [
+    './test_data/dest1',
+    './test_data/dest2'
+]
+
+exclude_file_types = [
+    '.sdf'
+]
+
+log_setup = (
+    './backup.log',
+    'ERROR'
+)
+
+notes = 'Backup test'
